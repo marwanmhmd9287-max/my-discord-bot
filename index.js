072471f3a320a9baedfa96fab6e6c4f4d859f431
marwanmhmd9287-max <marwanmhmd9287@gmail.com>
2026-09-24 15:57:50 +0300

    Create index.js

---
+++ index.js
@@ -0,0 +1,28 @@
+const { Client, GatewayIntentBits } = require('discord.js');
+const express = require('express');
+
+const app = express();
+app.get('/', (req, res) => res.send('Bot is Online!'));
+app.listen(8080, () => console.log('Server is ready!'));
+
+const client = new Client({
+    intents: [
+        GatewayIntentBits.Guilds,
+        GatewayIntentBits.GuildMessages,
+        GatewayIntentBits.MessageContent
+    ]
+});
+
+client.on('ready', () => {
+    console.log(`Logged in as ${client.user.tag}!`);
+});
+
+client.on('messageCreate', (message) => {
+    if (message.author.bot) return;
+    if (message.content === '!ping') {
+        message.reply('Pong! 🏓');
+    }
+});
+
+client.login(process.env.DISCORD_TOKEN);
+
